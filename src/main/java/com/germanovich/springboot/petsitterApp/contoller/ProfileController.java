eca383877ba2234443e7a4240e7d9943065f7280
Germanovich Pavel <germanovichpavel95@gmail.com>
2022-07-02 12:56:32 +0300

    added petowner profile page

--- src/main/java/com/germanovich/springboot/petsitterApp/contoller/ProfileController.java
+++ src/main/java/com/germanovich/springboot/petsitterApp/contoller/ProfileController.java
@@ -1,7 +1,9 @@
 package com.germanovich.springboot.petsitterApp.contoller;
 
+import com.germanovich.springboot.petsitterApp.dao.CityRepository;
 import com.germanovich.springboot.petsitterApp.dao.PetOwnerRepository;
 import com.germanovich.springboot.petsitterApp.dao.PetsitterRepository;
+import com.germanovich.springboot.petsitterApp.entity.City;
 import com.germanovich.springboot.petsitterApp.entity.PetOwner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 public class ProfileController {
@@ -18,13 +23,22 @@ public class ProfileController {
     private PetsitterRepository petsitterRepository;
 
     @Autowired
+    private CityRepository cityRepository;
+
+    @Autowired
     private PetOwnerRepository petOwnerRepository;
+
+    private List<City> getCityList() {
+        return StreamSupport.stream(cityRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
 
     @ModelAttribute
     @PreAuthorize("hasAnyAuthority('PET_OWNER')")
     public void getUserCommonData(Model model, Principal principal) {
         PetOwner petOwner = petOwnerRepository.findPetOwnerByUserEmail(principal.getName());
         model.addAttribute("petowner", petOwner);
+        model.addAttribute("cityList", getCityList());
     }
 
     @GetMapping("/user")
